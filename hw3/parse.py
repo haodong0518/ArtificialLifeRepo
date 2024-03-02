4a17e6c983b52ed860db1599b03b4244068047e4
haodong0518 <hwang856@wisc.edu>
2024-03-02 16:46:40 -0600

    hw3_submit

--- hw3/parse.py
+++ hw3/parse.py
@@ -1,5 +1,5 @@
 import xml.dom.minidom
 
-dom = xml.dom.minidom.parse("output4.XML") # or xml.dom.minidom.parseString(xml_string)
+dom = xml.dom.minidom.parse("actuator.XML") # or xml.dom.minidom.parseString(xml_string)
 pretty_xml_as_string = dom.toprettyxml()
 print(pretty_xml_as_string)
