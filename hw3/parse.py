d26f44f3b85f3f61942d6ef510a1b5644ba7aca6
haodong0518 <99232125+haodong0518@users.noreply.github.com>
2024-03-14 18:17:17 -0500

    Delete hw3 directory

--- hw3/parse.py
+++
@@ -1,5 +0,0 @@
-import xml.dom.minidom
-
-dom = xml.dom.minidom.parse("actuator.XML") # or xml.dom.minidom.parseString(xml_string)
-pretty_xml_as_string = dom.toprettyxml()
-print(pretty_xml_as_string)
